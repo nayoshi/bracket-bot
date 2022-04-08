d646f7dd01322b0cc66c898982fbf5bbfce27ba8
nayoshi <nayoshi12@gmail.com>
2022-04-08 02:40:49 -0400

    Added dotenv for api keys

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
-const {Client, MessageEmbed} = require('discord.js');
+require('dotenv').config()
 
-const client = new Client();
-var data = {}
+// const {Client, MessageEmbed} = require('discord.js');
 
-client.on('ready', () => {
-  console.log('Apex Bot is ready!')
+// const client = new Client();
+// var data = {}
 
-})
+// client.on('ready', () => {
+//   console.log('Apex Bot is ready!')
 
-function GrabJSONFromURL() : String {
-    return ""
-}
+// })
 
-GrabJSONFromURL = async(link) => {
-  try{
-    let response = await timeout(4000, fetch("http://" + config.BaseURL + ":" + config.Port + "/" + link + ".json"))
-    let body = await response.text()
-    return JSON.parse(body)
-  } catch {
-    return null
-  }
-}
+// function GrabJSONFromURL() : String {
+//     return ""
+// }
+
+// GrabJSONFromURL = async(link) => {
+//   try{
+//     let response = await timeout(4000, fetch("http://" + config.BaseURL + ":" + config.Port + "/" + link + ".json"))
+//     let body = await response.text()
+//     return JSON.parse(body)
+//   } catch {
+//     return null
+//   }
+// }
+
+console.log(process.env['S3_BUCKET']) // remove this after you've confirmed it working
